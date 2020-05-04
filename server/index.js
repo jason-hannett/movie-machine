89b0276072eddf12aeec1b171ca9cda9eafa151c
Aidan Trujillo <aidantrujillo@Aidans-MBP.hsd1.ut.comcast.net>
2020-05-04 12:38:23 -0600

    basic server setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express'),
+      massive = require('massive'),
+      session = require('express-session'),
+      {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
+      port = SERVER_PORT
+      app = express()
+
+app.use(express.json())
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60}
+}))
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db)
+    console.log('db connected')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
