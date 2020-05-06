6408f9767bd2397439a1d7bf6d6e320383b4d05f
Rob-Swensen <rg.swensen801@gmail.com>
2020-05-06 12:47:45 -0600

    Page numbers for movielist

--- src/Components/MovieList/MovieList.js
+++ src/Components/MovieList/MovieList.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import DisplayMovie from "./DisplayList";
 
 function MovieList(props) {
-  return <div>MovieList Component</div>;
+  const [movieList, setMovieList] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const previousPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
+  function usePrevious(){
+    const ref = useRef()
+    useEffect(() => {
+      ref.current = props.match.params.list
+    })
+    return ref.current
+  }
+
+  const prevList = usePrevious();
+
+  console.log(pageNumber);
+  useEffect(() => {
+    if(prevList !== props.match.params.list){
+      axios
+      .get(`/api/movies/?list=${props.match.params.list}&page=1`)
+      .then((response) => {
+        setMovieList(response.data);
+        setPageNumber(1)
+      });
+    } else {
+      axios
+        .get(`/api/movies/?list=${props.match.params.list}&page=${pageNumber}`)
+        .then((response) => {
+          setMovieList(response.data);
+        });
+    }
+  }, [props.match.params.list, pageNumber]);
+
+  let displayList = movieList.map((movie, index) => {
+    return <DisplayMovie key={index} movie={movie} />;
+  });
+  return (
+    <div>
+      {displayList}
+      <button onClick={previousPage}>Previous Page</button>
+      <p>{pageNumber}</p>
+      <button onClick={() => setPageNumber(pageNumber + 1)}>Next Page</button>
+    </div>
+  );
 }
 
 export default MovieList;
