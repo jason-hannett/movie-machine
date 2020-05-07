448f7fb0e191c83fdd82a7ef5d49280aff0ced31
Aidan Trujillo <aidantrujillo@Aidans-MBP.hsd1.ut.comcast.net>
2020-05-07 16:48:24 -0600

    landing page displays categories/routes to movies

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -4,15 +4,26 @@ import './Landing.scss'
 import axios from 'axios'
 
 function Landing(props) {
-  console.log(props)
-  const [movies, setMovies] = useState([])
+  const [popularMovies, setPopular] = useState([])
+  const [topRated, setTopRated] = useState([])
+  const [nowPlaying, setNowPlaying] = useState([])
   const [randomMovie, setRandom] = useState([])
   const [toggleRandom, setToggleRandom] = useState(false)
 
   useEffect(() => {
-    axios.get(`/api/latest-movies`)
+    axios.get(`/api/now-playing`)
     .then(response => {
-      setMovies(response.data)
+      console.log(response)
+      setNowPlaying(response.data.results)
+      axios.get('/api/top-rated')
+        .then(response => {
+          console.log(response)
+          setTopRated(response.data.results)
+          axios.get('/api/popular')
+            .then(response => {
+              setPopular(response.data.results)
+            })
+        })
     })
     .catch(err => console.log(err))
   }, [])
@@ -29,43 +40,82 @@ function Landing(props) {
         })
   }
 
-  console.log(randomMovie)
+  console.log(popularMovies)
+  console.log(topRated)
+  console.log(nowPlaying)
 
 
+  const mappedNP = nowPlaying.map((e, index) => {
+    return (
+      <div key={index} className='mapped-movie' onClick={() => {props.history.push(`/movie/${e.id}`)}}>
+        <img src={`https://image.tmdb.org/t/p/w500${e.poster_path}`} alt={e.title}/>
+        <div className='mapmovie-info'>
+          <p>{e.vote_average}</p>
+          <p>{e.title}</p>
+          <button>+ Watchlist</button>
+        </div>
+      </div>
+    )
+  })
+
+  const mappedTop = topRated.map((e, index) => {
+    return (
+      <div key={index} className='mapped-movie' onClick={() => {props.history.push(`/movie/${e.id}`)}}>
+        <img src={`https://image.tmdb.org/t/p/w500${e.poster_path}`} alt={e.title}/>
+        <div className='mapmovie-info'>
+          <p>{e.vote_average}</p>
+          <p>{e.title}</p>
+          <button>+ Watchlist</button>
+        </div>
+      </div>
+    )
+  })
+
+  const mappedPop = popularMovies.map((e, index) => {
+    return (
+      <div key={index} className='mapped-movie' onClick={() => {props.history.push(`/movie/${e.id}`)}}>
+        <img src={`https://image.tmdb.org/t/p/w500${e.poster_path}`} alt={e.title}/>
+        <div className='mapmovie-info'>
+          <p>{e.vote_average}</p>
+          <p>{e.title}</p>
+          <button>+ Watchlist</button>
+        </div>
+      </div>
+    )
+  })
 
     return(
       <div className='landing-page'>
 
         <button onClick={() => handleRandom()}>Random Movie</button>
         {toggleRandom ?
-                    <div>
+                    <div className='random-movie'>
                       <img src={`https://image.tmdb.org/t/p/w500${randomMovie.poster_path}`} alt='poster' onClick={() => {
                         props.history.push(`/movie/${randomMovie.id}`)
                       }}/>
                     
                       <p>Title: {randomMovie.title}</p>
+                      <p>Rating: {randomMovie.vote_average}</p>
                       <p>Release Date: {randomMovie.release_date}</p>
                     </div>
                        :
                       null}
 
         <div className='movie-display'>
-          <p>Recommended</p>
+          <h2>Now Playing</h2>
           <div className='display-sections'>
+            {mappedNP}
+          </div>
 
-
+          <h2>Top Rated</h2>
+          <div className='display-sections'>
+            {mappedTop}
 
           </div>
 
-          <p>Test</p>
+          <h2>Popular</h2>
           <div className='display-sections'>
-            
-
-          </div>
-
-          <p>Recommended Today</p>
-          <div className='display-sections'>
-            
+            {mappedPop}
 
           </div>
         </div>
